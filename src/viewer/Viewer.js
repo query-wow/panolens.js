270c54facf7c4db525bf68f5810ceb1c6575831a
query-wow <660776+query-wow@users.noreply.github.com>
2020-10-13 18:11:22 +0100

    GetPosition Method

--- src/viewer/Viewer.js
+++ src/viewer/Viewer.js
@@ -298,6 +298,25 @@ Viewer.prototype = Object.assign( Object.create( THREE.EventDispatcher.prototype
 
     },
 
+    getPosition: function () {
+        var intersects, point, panoramaWorldPosition, outputPosition;
+        intersects = this.raycaster.intersectObject( this.panorama, true );
+    
+        if ( intersects.length > 0 ) {
+            point = intersects[0].point;
+            panoramaWorldPosition = this.panorama.getWorldPosition();
+    
+            // Panorama is scaled -1 on X axis
+            outputPosition = new THREE.Vector3(
+                -(point.x - panoramaWorldPosition.x).toFixed(2),
+                (point.y - panoramaWorldPosition.y).toFixed(2),
+                (point.z - panoramaWorldPosition.z).toFixed(2)
+            );
+        }
+    
+        return outputPosition;
+    },
+
     /**
      * Remove an object from the scene
      * @param  {THREE.Object3D} object - Object to be removed
